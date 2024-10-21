7d0ea2d7c66501bf6712de9b5b76ed5bc5aa1c18
yohangoffermann <yohangoffermann@gmail.com>
2024-10-21 17:41:09 -0300

    Update cenarios.py

--- cenarios.py
+++ cenarios.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
 import numpy as np
+import plotly.graph_objects as go
 
 def calculate_auto_financiado(vgv, custo_construcao, prazo_meses, entrada_percentual):
     fluxo = pd.DataFrame(index=range(prazo_meses), columns=['Receitas', 'Custos', 'Saldo'])
